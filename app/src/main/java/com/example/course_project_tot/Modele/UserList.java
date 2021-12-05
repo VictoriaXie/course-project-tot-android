069c62d7b499ef7f4670adb6c2655cf9f7b3ea92
victoriaxie <yushan.xie@mail.utoronto.ca>
2021-12-05 14:10:45 -0500

    store and extract data

--- app/src/main/java/com/example/course_project_tot/Modele/UserList.java
+++ app/src/main/java/com/example/course_project_tot/Modele/UserList.java
@@ -1,7 +1,16 @@
 package com.example.course_project_tot.Modele;
 
+import com.google.gson.Gson;
+import com.jjoe64.graphview.series.DataPoint;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UserList implements Serializable {
@@ -26,3 +35,27 @@ public class UserList implements Serializable {
     }
 }
 
+class GsonWriteList {
+
+    public static void main(String[] args) throws IOException {
+
+        String fileName = "src/main/resources/items.json";
+
+        try (FileOutputStream fos = new FileOutputStream(fileName);
+             OutputStreamWriter isr = new OutputStreamWriter(fos,
+                     StandardCharsets.UTF_8)) {
+
+            Gson gson = new Gson();
+
+            UserList item1 = new UserList();
+            UserList item2 = new UserList();
+            UserList item3 = new UserList();
+
+            List<UserList> items = new ArrayList<>();
+            items.add(item1);
+            items.add(item2);
+            items.add(item3);
+
+            gson.toJson(items, isr);
+        }
+    }}
